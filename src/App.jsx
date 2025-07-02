60b32b4357302803bd871f7fee80dd091944da69
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-07-01 23:53:47 -0300

    agregue footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
-  
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <main className="container my-3">
+        <h1 className="text-center">Hola Mundo</h1>
+      </main>
+      <footer className="bg-info text-light text-center py-3">
+        <p>&copy;Todos los derechos reservados</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
