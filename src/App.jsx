f18aa4e8c6c3e3cdb8946ff96df2c4a99574b3ae
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-07-02 00:07:50 -0300

    maquetado del form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Formulario from "./components/Formulario";
 
 function App() {
   return (
     <>
       <main className="container my-3">
         <h1 className="text-center">Hola Mundo</h1>
+        <Formulario/>
       </main>
       <footer className="bg-info text-light text-center py-3">
         <p>&copy;Todos los derechos reservados</p>
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,76 @@
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useForm } from "react-hook-form";
+
+const Formulario = () => {
+
+    const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+    return (
+        <section className="container">
+            <div>
+                <Form onSubmit={handleSubmit} className="mb-4">
+        <Form.Group className="mb-3 row justify-content-center gap-2">
+          <Form.Control
+            type="text"
+            placeholder="Ingresa una ciudad"
+            {...register("inputCiudad", {
+              required: "La ciudad es un dato obligatorio",
+              minLength: {
+                value: 3,
+                message: "La ciudad debe tener 3 caracteres como minimo",
+              },
+              maxLength: {
+                value: 50,
+                message: "La ciudad debe tener 50 caracteres como minimo",
+              },
+              pattern: {
+                value: /^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,50}$/,
+                message:
+                  "La ciudad debe contener caracteres alfa, mayusculas o minusculas",
+              },
+            })} className="col-12 col-md-6"
+          />
+          <Form.Text className="text-danger">
+          {errors.inputCiudad?.message}
+        </Form.Text>
+          <Form.Control
+            type="text"
+            placeholder="Ingresa un pais"
+            {...register("inputPais", {
+              required: "El pais es un dato obligatorio",
+              minLength: {
+                value: 3,
+                message: "El pais debe tener 3 caracteres como minimo",
+              },
+              maxLength: {
+                value: 50,
+                message: "El pais debe tener 50 caracteres como minimo",
+              },
+              pattern: {
+                value: /^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,50}$/,
+                message:
+                  "El pais debe contener caracteres alfa, mayusculas o minusculas",
+              },
+            })} className="col-12 col-md-6"
+          />
+          
+        <Form.Text className="text-danger">
+          {errors.inputPais?.message}
+        </Form.Text>
+        </Form.Group>
+        <Button variant="success" type="submit">
+            Consultar clima
+          </Button>
+      </Form>
+            </div>
+        </section>
+    );
+};
+
+export default Formulario;
